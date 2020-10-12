a701574e259d02e09e7992319e7c613b982e4c7b
Olavo <olavo1611@gmail.com>
2020-10-12 15:15:18 -0300

    Mecânicas finalizadas! Falta adição de cutscenes, botão de missão, música e animações

---
+++ Assets/Collider_script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider_script : MonoBehaviour
+{
+    public int count;
+
+    void start(){
+        count = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.CompareTag("Collectable")) //If thing hit is tagged "Obstacle"
+        {
+            Destroy(col.gameObject); //Then destroy the player
+            count ++;
+            print(count);
+        }
+        else if(col.gameObject.CompareTag("Launch_base")) //If thing hit is tagged "Obstacle"
+        {
+            if(count ==  4){
+                print("Você venceu!!");
+            } else{
+                print("Colete os objetos faltantes");
+            }
+        }
+    }
+}
+
